2b0b05f057ec17dff85b1f72a5564a5f29a5e2a9
Suphanat <pppppppppp102014@hotmail.com>
2022-04-01 00:42:41 +0700

    :guitar: start

---
+++ src/Themes/colors.ts
@@ -0,0 +1,10 @@
+export class ColorThemes {
+  static readonly light = {
+    primary: "#5077B0",
+    secondary: "#000E7B",
+    warning: "#E36B6B",
+    success: "#EED9A7",
+    light: "#F0F0F0",
+    dark: "#3E3E3E",
+  };
+}
