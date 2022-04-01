70622bd449e9ea82b5ab80082794dbde0a627854
กีตาร์ยังไงล่ะ <45527907+dudkinox@users.noreply.github.com>
2022-04-01 14:22:03 +0700

    confix

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import MicIcon from "../../assets/icons/mic-icon.svg";
 import SpaceIcon from "../../assets/icons/space-icon";
 import Icons from "../../components/Icons";
